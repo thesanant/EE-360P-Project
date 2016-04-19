cce3a804fac6af391381cf47b6859b1bc0dc2297
victordoval <victordoval@utexas.edu>
2016-04-19 00:09:50 -0500

    This is the Chatroom skeleton.

---
+++ Chatroom/src/Server.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.net.*;
+
+public class Server{
+
+	static ServerSocket serverSocket;
+	static Socket socket;
+	static DataOutputStream out;
+	static DataInputStream in;
+	static Users[] user = new Users[10];
+	
+	public static void main(String[] args) throws Exception{
+		System.out.println("Starting Server...");
+		serverSocket = new ServerSocket(7777);
+		System.out.println("Server Started...");
+		while(true){
+			socket = serverSocket.accept();
+			for(int i = 0; i < 10; i++){
+				System.out.println("Connection From: " + socket.getInetAddress());
+				out = new DataOutputStream(socket.getOutputStream());
+				in = new DataInputStream(socket.getInputStream());
+				if(user[i] == null){
+					user[i] = new Users(out,in,user);
+					Thread thread = new Thread(user[i]);
+					thread.start();
+					break;
+				}
+			}
+		}
+	}
+}
+
+class Users implements Runnable{
+
+	DataOutputStream out;
+	DataInputStream in;
+	Users[] user = new Users[10];
+	String name;
+
+	public Users(DataOutputStream out, DataInputStream in, Users[] user){
+		this.out = out;
+		this.in = in;
+		this.user = user;
+	}
+
+	public void run(){
+		try{
+			name = in.readUTF();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		while(true){
+			try{
+				String message = in.readUTF();
+				for(int i = 0; i < 10; i++){
+					if(user[i] != null){
+						user[i].out.writeUTF(name + ": " + message);
+					}
+				}
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
